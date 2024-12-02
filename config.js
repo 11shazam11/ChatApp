1f144e884b3746d4308308a9cc3ca9ab690e8cfb
11shazam11 <105714122+11shazam11@users.noreply.github.com>
2024-12-02 11:03:26 +0530

    config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const mongoConfig = ()=>{
-    mongoose.connect("mongodb://localhost:27017/chatting").then(()=>{
+    mongoose.connect("mongodb+srv://dhumneabhay:l4FVmLTfsLNRZDvv@cluster0.uoo3q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(()=>{
         console.log("Connected to mongoDB");
     })
 }
